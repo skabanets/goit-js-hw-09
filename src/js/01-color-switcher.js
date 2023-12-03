01031c16f8c37ccaa7fa9b550f6b10e04d871960
Sergey Kabanets <skabanets97@gmail.com>
2023-12-04 00:01:58 +0200

    add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
+const changeBackgroundColor = () => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+};
+
+const onButtonStartClick = () => {
+  refs.btnStart.setAttribute('disabled', '');
+  timerId = setInterval(changeBackgroundColor, 1000);
+};
+
+const onButtonStopClick = () => {
+  refs.btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+};
+
+refs.btnStart.addEventListener('click', onButtonStartClick);
+refs.btnStop.addEventListener('click', onButtonStopClick);
